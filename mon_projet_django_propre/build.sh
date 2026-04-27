42c6f5ada506633a32308a3e661c23791bd020de
VOTRE_NOM <votre-email@example.com>
2026-04-27 14:38:44 +0300

    Correction script bootstrap admin

--- mon_projet_django_propre/build.sh
+++ mon_projet_django_propre/build.sh
@@ -2,19 +2,13 @@
 
 echo "🚀 Début du build..."
 
-# Mettre à jour pip
 pip install --upgrade pip
-
-# Installer les dépendances
 pip install -r requirements.txt
 
-# Appliquer les migrations
 python manage.py migrate --noinput
-
-# Collecter les fichiers statiques
 python manage.py collectstatic --noinput
 
-# ⚠️ CRÉER LES COMPTES UTILISATEURS
+# ⚠️ CRITIQUE - CRÉER LES UTILISATEURS
 python bootstrap.py
 
 echo "✅ Build terminé !"
